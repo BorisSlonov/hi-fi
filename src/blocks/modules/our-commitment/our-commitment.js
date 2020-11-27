918dfa188d0037d268a718db407b28a450b575b1
Boris <sslonovborisss@gmail.com>
2020-11-27 15:56:53 +0300

    add commitment

---
+++ src/blocks/modules/our-commitment/our-commitment.js
@@ -0,0 +1,21 @@
+
+
+
+$(document).ready(function () {
+
+
+    function equalHeight(group) {
+        var tallest = 0;
+        group.each(function () {
+            thisHeight = $(this).height();
+            if (thisHeight > tallest) {
+                tallest = thisHeight;
+            }
+        });
+        group.height(tallest);
+    }
+
+    equalHeight($(".column-one-height"));
+
+
+}); 
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -4,6 +4,8 @@ import "%modules%/header/scroll-out";
 
 import "%modules%/clients/clients"
 
+import "%modules%/our-commitment/our-commitment"
+
 
 import "%modules%/footer/footer";
 
