ff3a7444d402f7f4775b22a653274d1e41b79cb8
Boris <sslonovborisss@gmail.com>
2020-11-30 15:21:53 +0300

    add

--- src/blocks/modules/sections/sections.js
+++ src/blocks/modules/sections/sections.js
@@ -20,23 +20,25 @@
 //   compositionCardText_3.classList.toggle("composition-card--active");
 // });
 
-// let labCard_1 = document.querySelector(".lab-card_1");
-// let labCardContent_1 = document.querySelector(".lab-card__content_1");
-// labCard_1.addEventListener("click", function (e) {
-//   labCardContent_1.classList.toggle("lab-card--active_1");
-// });
-// let labCard_2 = document.querySelector(".lab-card_2");
-// let labCardContent_2 = document.querySelector(".lab-card__content_2");
-// labCard_2.addEventListener("click", function (e) {
-//   labCardContent_2.classList.toggle("lab-card--active_2");
-// });
-// let labCard_3 = document.querySelector(".lab-card_3");
-// let labCardContent_3 = document.querySelector(".lab-card__content_3");
-// labCard_3.addEventListener("click", function (e) {
-//   labCardContent_3.classList.toggle("lab-card--active_1");
-// });
-// let labCard_4 = document.querySelector(".lab-card_4");
-// let labCardContent_4 = document.querySelector(".lab-card__content_4");
-// labCard_4.addEventListener("click", function (e) {
-//   labCardContent_4.classList.toggle("lab-card--active_2");
-// });
+
+
+//     let labCard_1 = document.querySelector(".lab-card_1");
+//     let labCardContent_1 = document.querySelector(".lab-card__content_1");
+//     labCard_1.addEventListener("click", function (e) {
+//         labCardContent_1.classList.toggle("lab-card--active_1");
+//     });
+//     let labCard_2 = document.querySelector(".lab-card_2");
+//     let labCardContent_2 = document.querySelector(".lab-card__content_2");
+//     labCard_2.addEventListener("click", function (e) {
+//         labCardContent_2.classList.toggle("lab-card--active_2");
+//     });
+//     let labCard_3 = document.querySelector(".lab-card_3");
+//     let labCardContent_3 = document.querySelector(".lab-card__content_3");
+//     labCard_3.addEventListener("click", function (e) {
+//         labCardContent_3.classList.toggle("lab-card--active_1");
+//     });
+//     let labCard_4 = document.querySelector(".lab-card_4");
+//     let labCardContent_4 = document.querySelector(".lab-card__content_4");
+//     labCard_4.addEventListener("click", function (e) {
+//         labCardContent_4.classList.toggle("lab-card--active_2");
+//     });
