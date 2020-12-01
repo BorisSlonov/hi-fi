d9207e4c38ede411373823b673da3fb046adfe14
Boris <sslonovborisss@gmail.com>
2020-12-01 16:28:49 +0300

    add

--- dist/js/main.js
+++ dist/js/main.js
@@ -307,6 +307,44 @@ $(document).ready(function () {
     $('.header__link_7').addClass('header__link_active');
   }
 });
+
+window.onload = function () {
+  var compositionCard_1 = document.querySelector(".composition-card_1");
+  var compositionCardText_1 = document.querySelector(".composition-card__text_1");
+  compositionCard_1.addEventListener("click", function (e) {
+    compositionCardText_1.classList.toggle("composition-card--active");
+  });
+  var compositionCard_2 = document.querySelector(".composition-card_2");
+  var compositionCardText_2 = document.querySelector(".composition-card__text_2");
+  compositionCard_2.addEventListener("click", function (e) {
+    compositionCardText_2.classList.toggle("composition-card--active");
+  });
+  var compositionCard_3 = document.querySelector(".composition-card_3");
+  var compositionCardText_3 = document.querySelector(".composition-card__text_3");
+  compositionCard_3.addEventListener("click", function (e) {
+    compositionCardText_3.classList.toggle("composition-card--active");
+  });
+  var labCard_1 = document.querySelector(".lab-card_1");
+  var labCardContent_1 = document.querySelector(".lab-card__content_1");
+  labCard_1.addEventListener("click", function (e) {
+    labCardContent_1.classList.toggle("lab-card--active_1");
+  });
+  var labCard_2 = document.querySelector(".lab-card_2");
+  var labCardContent_2 = document.querySelector(".lab-card__content_2");
+  labCard_2.addEventListener("click", function (e) {
+    labCardContent_2.classList.toggle("lab-card--active_2");
+  });
+  var labCard_3 = document.querySelector(".lab-card_3");
+  var labCardContent_3 = document.querySelector(".lab-card__content_3");
+  labCard_3.addEventListener("click", function (e) {
+    labCardContent_3.classList.toggle("lab-card--active_1");
+  });
+  var labCard_4 = document.querySelector(".lab-card_4");
+  var labCardContent_4 = document.querySelector(".lab-card__content_4");
+  labCard_4.addEventListener("click", function (e) {
+    labCardContent_4.classList.toggle("lab-card--active_2");
+  });
+};
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
 /***/ }),
