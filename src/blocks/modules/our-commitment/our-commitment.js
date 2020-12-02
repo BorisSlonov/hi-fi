c030c2d985e9ea1dc0cf741d87637a7ea03c13cd
Boris <sslonovborisss@gmail.com>
2020-12-02 11:34:24 +0300

    add

--- src/blocks/modules/our-commitment/our-commitment.js
+++ src/blocks/modules/our-commitment/our-commitment.js
@@ -4,21 +4,26 @@
 $(document).ready(function () {
 
 
-    function equalHeight(group) {
-        var tallest = 0;
-        group.each(function () {
-            thisHeight = $(this).height();
-            if (thisHeight > tallest) {
-                tallest = thisHeight;
-            }
-        });
-        group.height(tallest);
+
+
+    if (window.innerWidth >= 768) {
+
+        function equalHeight(group) {
+            var tallest = 0;
+            group.each(function () {
+                thisHeight = $(this).height();
+                if (thisHeight > tallest) {
+                    tallest = thisHeight;
+                }
+            });
+            group.height(tallest);
+        }
+        equalHeight($(".column-one-height"));
+
     }
 
-    equalHeight($(".column-one-height"));
 
 
 
 });
-
 
