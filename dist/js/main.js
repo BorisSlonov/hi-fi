f2fbe9c0195ad818d3978cccd11b764a9c3e0a93
Boris <sslonovborisss@gmail.com>
2020-12-03 08:17:22 +0300

    end animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -162,17 +162,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// let test = getComputedStyle(document.querySelector('.main-btn'), ':before');
-// test.addEventListener("mouseenter", function (event) {
-//     setTimeout(function () {
-//         event.target.style.display = "none";
-//     }, 700);
-// }, false);
-// test.addEventListener("mouseover", function (event) {
-//     setTimeout(function () {
-//         event.target.style.display = "block";
-//     }, 700);
-// }, false);
+
 
 /***/ }),
 
