322d75e96a3cffd34e73e5177522dbcf907996a2
Boris <sslonovborisss@gmail.com>
2020-12-03 03:53:04 +0300

    add popups

--- dist/js/main.js
+++ dist/js/main.js
@@ -162,17 +162,17 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var test = getComputedStyle(document.querySelector('.main-btn'), ':before');
-test.addEventListener("mouseenter", function (event) {
-  setTimeout(function () {
-    event.target.style.display = "none";
-  }, 700);
-}, false);
-test.addEventListener("mouseover", function (event) {
-  setTimeout(function () {
-    event.target.style.display = "block";
-  }, 700);
-}, false);
+// let test = getComputedStyle(document.querySelector('.main-btn'), ':before');
+// test.addEventListener("mouseenter", function (event) {
+//     setTimeout(function () {
+//         event.target.style.display = "none";
+//     }, 700);
+// }, false);
+// test.addEventListener("mouseover", function (event) {
+//     setTimeout(function () {
+//         event.target.style.display = "block";
+//     }, 700);
+// }, false);
 
 /***/ }),
 
