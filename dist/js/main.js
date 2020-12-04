bc591d5a444afeaa0e1009990025f62dacdc4d64
Boris <sslonovborisss@gmail.com>
2020-12-04 10:26:48 +0300

    add

--- dist/js/main.js
+++ dist/js/main.js
@@ -710,7 +710,7 @@ function parallaxsectorsInit() {
     orientation: 'down',
     overflow: true,
     delay: .9,
-    scale: 1.5,
+    scale: 1.8,
     transition: 'cubic-bezier(0.25, 0.1, 0.25, 1.0)'
   });
   var sectorsAnimation_2 = document.querySelector(".sectors-animation_2");
@@ -725,16 +725,16 @@ function parallaxsectorsInit() {
   new simpleParallax(sectorsAnimation_3, {
     orientation: 'right',
     overflow: true,
-    delay: .9,
-    scale: 1.9,
+    delay: 1.2,
+    scale: 1.6,
     transition: 'cubic-bezier(0.25, 0.1, 0.25, 1.0)'
   });
   var sectorsAnimation_4 = document.querySelector(".sectors-animation_4");
   new simpleParallax(sectorsAnimation_4, {
     orientation: 'down left',
     overflow: true,
-    delay: .9,
-    scale: 1.3,
+    delay: 1.2,
+    scale: 1.6,
     transition: 'cubic-bezier(0.25, 0.1, 0.25, 1.0)'
   });
   var sectorsAnimation_5 = document.querySelector(".sectors-animation_5");
@@ -787,8 +787,73 @@ function parallaxsectorsInit() {
   });
 }
 
-parallaxsectorsInit();
-setTimeout(parallaxsectorsInit(), 1000);
+parallaxsectorsInit(); //del animate
+
+var sectorsAnimation_1 = document.querySelector(".sectors-animation_1");
+
+if (sectorsAnimation_1) {
+  sectorsAnimation_1.addEventListener('animationend', function () {
+    sectorsAnimation_1.classList.remove('animate__animated', 'animate__fadeInDown', 'animate__delay-3s');
+  });
+}
+
+var sectorsAnimation_2 = document.querySelector(".sectors-animation_2");
+
+if (sectorsAnimation_2) {
+  sectorsAnimation_2.addEventListener('animationend', function () {
+    sectorsAnimation_2.classList.remove('animate__animated', 'animate__fadeInLeft', 'animate__delay-2s');
+  });
+}
+
+var sectorsAnimation_3 = document.querySelector(".sectors-animation_3");
+
+if (sectorsAnimation_3) {
+  sectorsAnimation_3.addEventListener('animationend', function () {
+    sectorsAnimation_3.classList.remove('animate__animated', 'animate__fadeInLeft', 'animate__delay-2s');
+  });
+}
+
+var sectorsAnimation_4 = document.querySelector(".sectors-animation_4");
+
+if (sectorsAnimation_4) {
+  sectorsAnimation_4.addEventListener('animationend', function () {
+    sectorsAnimation_4.classList.remove('animate__animated', 'animate__fadeInRight', 'animate__delay-2s');
+  });
+}
+
+var sectorsAnimation_5 = document.querySelector(".sectors-animation_5");
+
+if (sectorsAnimation_5) {
+  sectorsAnimation_5.addEventListener('animationend', function () {
+    sectorsAnimation_5.classList.remove('animate__animated', 'animate__fadeInDown', 'animate__delay-2s');
+  });
+}
+
+var sectorsAnimation_6 = document.querySelector(".sectors-animation_6");
+
+if (sectorsAnimation_6) {
+  sectorsAnimation_6.addEventListener('animationend', function () {
+    sectorsAnimation_6.classList.remove('animate__animated', 'animate__fadeInDown', 'animate__delay-2s');
+  });
+}
+
+var sectorsAnimation_7 = document.querySelector(".sectors-animation_7");
+
+if (sectorsAnimation_7) {
+  sectorsAnimation_7.addEventListener('animationend', function () {
+    sectorsAnimation_7.classList.remove('animate__animated', 'animate__fadeInDown', 'animate__delay-2s');
+  });
+}
+
+var sectorsAnimation_9 = document.querySelector(".sectors-animation-tr_2");
+
+if (sectorsAnimation_9) {
+  sectorsAnimation_9.addEventListener('animationend', function () {
+    sectorsAnimation_9.classList.remove('animate__animated', 'animate__zoomInUp', 'animate__delay-1s');
+  });
+}
+
+var sectorsAnimation_10 = document.querySelector(".sectors-animation-tr_1");
 
 /***/ }),
 
